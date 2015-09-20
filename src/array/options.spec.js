df2c511ad95a00a9328ab3927dab6181f99aead9
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-20 10:08:02 -0700

    test defaults

--- src/array/options.spec.js
+++ src/array/options.spec.js
@@ -19,4 +19,27 @@ test('should throw when receiving non-object literal options', t => {
   t.end()
 });
 
+test('should have defaults', t => {
+  let options = ChunkifyOptions.of({});
+  t.equals(typeof options.complete, 'function');
+  t.equals(typeof options.delay, 'number');
+  t.equals(typeof options.chunk, 'number');
+  t.end()
+});
+
+test.skip('should override defaults', t => {
+
+});
+
+test.skip('should throw when an option override has the wrong type', t => {
+
+});
+
+test.skip('should be read-only', t => {
+
+});
+
+
+
+
 
