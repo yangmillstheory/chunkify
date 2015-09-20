7d6fcc110160381e7b69f9d5f3d4f6ac6c0c284e
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-20 13:55:17 -0700

    skip array tests

--- src/array.spec.js
+++ src/array.spec.js
@@ -1,46 +1,46 @@
 import test from 'tape'
 
 
-test('should require an array', t => {
+test.skip('should require an array', t => {
 
 });
 
-test('should require a function', t => {
+test.skip('should require a function', t => {
 
 });
 
-test('should delegate options deserialization', t => {
+test.skip('should delegate options deserialization', t => {
 
 });
 
-test('should return a promise', t => {
+test.skip('should return a promise', t => {
 
 });
 
-test('should not invoke fn when given an empty array', t => {
+test.skip('should not invoke fn when given an empty array', t => {
 
 });
 
-test('should invoke fn successively between "chunk" iterations', t => {
+test.skip('should invoke fn successively between "chunk" iterations', t => {
 
 });
 
-test('should yield after "chunk" iterations', t => {
+test.skip('should yield after "chunk" iterations', t => {
 
 });
 
-test('should start again after yielding in "delay" milliseconds', t => {
+test.skip('should start again after yielding in "delay" milliseconds', t => {
 
 });
 
-test('should resolve with the array after processing completes', t => {
+test.skip('should resolve with the array after processing completes', t => {
 
 });
 
-test('should reject the promise if an error is thrown', t => {
+test.skip('should reject the promise if an error is thrown', t => {
 
 });
 
-test('should not yield after "chunk" iterations if processing is complete', t => {
+test.skip('should not yield after "chunk" iterations if processing is complete', t => {
 
 });
