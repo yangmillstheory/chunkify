f35dc3f0b4e0a2c80fba2afaff75bef81e670094
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-20 12:50:39 -0700

    empty tests

---
+++ src/array.spec.js
@@ -0,0 +1,46 @@
+import test from 'tape'
+
+
+test('should require an array', t => {
+
+});
+
+test('should require a function', t => {
+
+});
+
+test('should delegate options deserialization', t => {
+
+});
+
+test('should return a promise', t => {
+
+});
+
+test('should not invoke fn when given an empty array', t => {
+
+});
+
+test('should invoke fn successively between "chunk" iterations', t => {
+
+});
+
+test('should yield after "chunk" iterations', t => {
+
+});
+
+test('should start again after yielding in "delay" milliseconds', t => {
+
+});
+
+test('should resolve with the array after processing completes', t => {
+
+});
+
+test('should reject the promise if an error is thrown', t => {
+
+});
+
+test('should not yield after "chunk" iterations if processing is complete', t => {
+
+});
