aa8a09dc57cff83a9aca10d6fe9ed21f31bef5ad
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-21 21:00:05 -0700

    Validate so programmer errors don't fail quietly

--- src/testutils.js
+++ src/testutils.js
@@ -1,5 +1,6 @@
 import sinon from 'sinon'
 import ChunkifyOptions from './options'
+import _ from 'underscore'
 
 
 let ChunkifyOptionsSpy = (callback) => {
@@ -8,6 +9,16 @@ let ChunkifyOptionsSpy = (callback) => {
 };
 
 let tick = ({before_tick, after_tick, delay}) => {
+  let ok_tick = () => {
+    if (!_.isFunction(before_tick)) {
+      throw new Error(`Expected before_tick function, got ${before_tick}`);
+    } else if (!_.isFunction(after_tick)) {
+      throw new Error(`Expected after_tick function, got ${after_tick}`);
+    } else if (!_.isNumber(delay)) {
+      throw new Error(`Expected number delay, got ${delay}`);
+    }
+  };
+  ok_tick();
   let clock = sinon.useFakeTimers();
   let before_tick_result = before_tick();
   clock.tick(delay);
