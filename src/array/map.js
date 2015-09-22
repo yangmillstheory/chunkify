01aa7c78fb9db549933056b9601c55071760de67
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-21 21:45:48 -0700

    map: when wrapping fn, bind to context.

--- src/array/map.js
+++ src/array/map.js
@@ -6,15 +6,16 @@ import {ok_usage} from './utilities'
 
 const USAGE = 'Usage: chunkify.map(Array array, Function fn, [Object options])';
 
-let map = (array, fn, options) => {
+let map = (array, fn, options = {}) => {
   ok_usage(array, fn, USAGE);
+  let okopts = ChunkifyOptions.of(options);
   let mapped = [];
   let pusher = (result) => {
     mapped.push(result);
     return result;
   };
-  let mapper = _.compose(pusher, fn);
-  return each.apply(array, mapper, options).then(() => {
+  let mapper = _.compose(pusher, fn.bind(okopts.scope));
+  return each.apply(array, mapper, okopts).then(() => {
     return mapped;
   });
 };
