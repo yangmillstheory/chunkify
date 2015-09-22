0f7c2da50f16090a730f5a1ea47f78ee464ed81e
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-21 22:57:38 -0700

    Fix main export; RELEASE 2.0.4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import 'babel/polyfill'
 import array from './array'
 
-export function each() {
-  return array.each(...arguments)
-}
-
-export function map() {
-  return array.map(...arguments)
-}
-
-export default {each, map}
+export default {each: array.each, map: array.map}
