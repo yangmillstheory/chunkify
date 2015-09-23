14d208d0e8d15c4be5bf0045841edfa9d886a9ed
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-22 22:07:05 -0700

    curried .each, specify bounds. call fn with index.

--- src/array/each.js
+++ src/array/each.js
@@ -5,30 +5,39 @@ import {ok_usage} from './utilities'
 
 const USAGE = 'Usage: chunkify.each(Array array, Function fn, [Object options])';
 
-let each = (array, fn, options = {}) => {
-  ok_usage(array, fn, USAGE);
-  let {chunk, delay, scope} = ChunkifyOptions.of(options);
-  let index = 0;
-  let total = array.length;
-  let incomplete = () => {
-    return index < total;
+let each = (start, final) => {
+  let reset = (array) => {
+    return _.defaults({index: start, final}, {
+      index: 0,
+      final: array.length
+    });
   };
-  var process_chunk = (resolve, reject) => {
-    while (incomplete()) {
-      let item = array[index++];
-      try {
-        fn.call(scope, item)
-      } catch (error) {
-        return reject({error, item});
+  return (array, fn, options = {}) => {
+    ok_usage(array, fn, USAGE);
+    let {chunk, delay, scope} = ChunkifyOptions.of(options);
+    let {index, final} = reset(array);
+    let incomplete = () => {
+      return index < final;
+    };
+    var process_chunk = (resolve, reject) => {
+      while (incomplete()) {
+        let item = array[index];
+        try {
+          fn.call(scope, item, index)
+        } catch (error) {
+          return reject({error, item, index});
+        }
+        index++;
+        if (incomplete() && ((index % chunk) === 0)) {
+          return setTimeout(process_chunk.bind(null, resolve, reject), delay);
+        }
       }
-      if (incomplete() && ((index % chunk) === 0)) {
-        return setTimeout(process_chunk.bind(null, resolve, reject), delay);
-      }
-    }
-    resolve();
-  } ;
-  return new Promise(process_chunk)
+      resolve();
+    } ;
+    return new Promise(process_chunk)
+  };
 };
 
 
-export default {apply: each}
+
+export default {apply: each(0), each}
