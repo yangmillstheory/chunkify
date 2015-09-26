133443d82b8dfed30b2b762727b3f9103c452673
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-25 21:52:52 -0700

    Remove needless complexity

--- src/array/each.js
+++ src/array/each.js
@@ -5,38 +5,32 @@ import {ok_usage} from './utilities'
 
 const USAGE = 'Usage: chunkify.each(Array array, Function fn, [Object options])';
 
-let bounded_each = ({from, to}) => {
-  return (array, fn, options = {}) => {
-    ok_usage(array, fn, USAGE);
-    let {chunk, delay, scope} = ChunkifyOptions.of(options);
-    let {index, final} = _.defaults({index: from, final: to}, {
-      index: 0,
-      final: array.length
-    });
-    let incomplete = () => {
-      return index < final;
-    };
-    var process_chunk = (resolve, reject) => {
-      while (incomplete()) {
-        let item = array[index];
-        try {
-          fn.call(scope, item, index)
-        } catch (error) {
-          return reject({error, item, index});
-        }
-        index++;
-        if (incomplete() && ((index % chunk) === 0)) {
-          return setTimeout(process_chunk.bind(null, resolve, reject), delay);
-        }
-      }
-      resolve();
-    } ;
-    return new Promise(process_chunk)
+let iterate = (array, fn, options = {}) => {
+  ok_usage(array, fn, USAGE);
+  let {chunk, delay, scope} = ChunkifyOptions.of(options);
+  let index = 0;
+  let final = array.length;
+  let incomplete = () => {
+    return index < final;
   };
+  var process_chunk = (resolve, reject) => {
+    while (incomplete()) {
+      let item = array[index];
+      try {
+        fn.call(scope, item, index)
+      } catch (error) {
+        return reject({error, item, index});
+      }
+      index++;
+      if (incomplete() && ((index % chunk) === 0)) {
+        return setTimeout(process_chunk.bind(null, resolve, reject), delay);
+      }
+    }
+    resolve();
+  } ;
+  return new Promise(process_chunk)
 };
 
 
 
-export default {
-  iterate: bounded_each({from: 0})
-}
+export default {iterate}
