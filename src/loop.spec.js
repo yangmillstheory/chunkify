1fa34abc089a6a615ba302e817d51969654a24dd
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-27 00:18:11 -0700

    Finish loop tests

--- src/loop.spec.js
+++ src/loop.spec.js
@@ -64,113 +64,113 @@ test('should invoke fn with the loop index', t => {
   })
 });
 
-//test('should invoke fn with the default scope', t => {
-//  let fn = sinon.spy();
-//
-//  chunkify.loop(['A', 'B', 'C'], fn, {chunk: 3}).then(() => {
-//    t.ok(fn.alwaysCalledOn(null));
-//    t.end()
-//  });
-//});
-//
-//test('should invoke fn with the provided scope', t => {
-//  let fn = sinon.spy();
-//  let scope = {};
-//
-//  chunkify.loop(['A', 'B', 'C'], fn, {chunk: 3, scope}).then(() => {
-//    t.ok(fn.alwaysCalledOn(scope));
-//    t.end()
-//  });
-//});
-//
-//test('should yield for at least `delay` ms after `chunk` iterations', t => {
-//  let fn = sinon.spy();
-//
-//  tick({
-//    delay: 999,
-//
-//    before_tick() {
-//      chunkify.loop(['A', 'B', 'C', 'D'], fn, {chunk: 3, delay: 1000});
-//      t.equals(fn.callCount, 3);
-//    },
-//
-//    after_tick() {
-//      // 1000 milliseconds hasn't elapsed yet
-//      t.equals(fn.callCount, 3);
-//      t.end();
-//    }
-//  });
-//});
-//
-//test('should start again in `delay` milliseconds after yielding', t => {
-//  let fn = sinon.spy();
-//
-//  tick({
-//    delay: 1000,
-//
-//    before_tick() {
-//      chunkify.loop(['A', 'B', 'C', 'D'], fn, {chunk: 3, delay: 1000});
-//      t.equals(fn.callCount, 3);
-//    },
-//
-//    after_tick() {
-//      t.equals(fn.callCount, 4);
-//      t.end();
-//    }
-//  });
-//});
-//
-//test('should resolve with undefined', t => {
-//  let fn = sinon.spy();
-//
-//  tick({
-//    delay: 1000,
-//
-//    before_tick() {
-//      let promise = chunkify.loop(['A', 'B', 'C', 'D'], fn, {chunk: 3, delay: 1000});
-//      t.equals(fn.callCount, 3);
-//      return promise
-//    },
-//
-//    after_tick(promise) {
-//      t.equals(fn.callCount, 4);
-//      promise.then((result) => {
-//        t.equals(result, undefined);
-//        t.end()
-//      })
-//    }
-//  });
-//});
-//
-//test('should reject the promise with rejection object and stop processing', t => {
-//  let error = {};
-//  let fn = sinon.spy((letter) => {
-//    if (letter === 'B') {
-//      throw error;
-//    }
-//  });
-//
-//  chunkify.loop(['A', 'B', 'C'], fn, {chunk: 3}).then(null, (rejection) => {
-//    t.deepEquals(rejection, {error, item: 'B', index: 1});
-//    t.equals(fn.callCount, 2);
-//    t.end()
-//  })
-//});
-//
-//test('should not yield after `chunk` iterations if processing is complete', t => {
-//  let fn = sinon.spy();
-//
-//  tick({
-//    delay: 2000,
-//
-//    before_tick() {
-//      chunkify.loop(['A', 'B', 'C'], fn, {chunk: 3, delay: 1000});
-//      t.equals(fn.callCount, 3)
-//    },
-//
-//    after_tick() {
-//      t.equals(fn.callCount, 3);
-//      t.end()
-//    }
-//  });
-//});
+test('should invoke fn with the default scope', t => {
+  let fn = sinon.spy();
+
+  chunkify.loop(3, fn, {chunk: 3}).then(() => {
+    t.ok(fn.alwaysCalledOn(null));
+    t.end()
+  });
+});
+
+test('should invoke fn with the provided scope', t => {
+  let fn = sinon.spy();
+  let scope = {};
+
+  chunkify.loop(3, fn, {chunk: 3, scope}).then(() => {
+    t.ok(fn.alwaysCalledOn(scope));
+    t.end()
+  });
+});
+
+test('should yield for at least `delay` ms after `chunk` iterations', t => {
+  let fn = sinon.spy();
+
+  tick({
+    delay: 999,
+
+    before_tick() {
+      chunkify.loop(4, fn, {chunk: 3, delay: 1000});
+      t.equals(fn.callCount, 3);
+    },
+
+    after_tick() {
+      // 1000 milliseconds hasn't elapsed yet
+      t.equals(fn.callCount, 3);
+      t.end();
+    }
+  });
+});
+
+test('should start again in `delay` milliseconds after yielding', t => {
+  let fn = sinon.spy();
+
+  tick({
+    delay: 1000,
+
+    before_tick() {
+      chunkify.loop(4, fn, {chunk: 3, delay: 1000});
+      t.equals(fn.callCount, 3);
+    },
+
+    after_tick() {
+      t.equals(fn.callCount, 4);
+      t.end();
+    }
+  });
+});
+
+test('should resolve with undefined', t => {
+  let fn = sinon.spy();
+
+  tick({
+    delay: 1000,
+
+    before_tick() {
+      let promise = chunkify.loop(4, fn, {chunk: 3, delay: 1000});
+      t.equals(fn.callCount, 3);
+      return promise
+    },
+
+    after_tick(promise) {
+      t.equals(fn.callCount, 4);
+      promise.then((result) => {
+        t.equals(result, undefined);
+        t.end()
+      })
+    }
+  });
+});
+
+test('should reject the promise with rejection object and stop processing', t => {
+  let error = {};
+  let fn = sinon.spy((index) => {
+    if (index === 1) {
+      throw error;
+    }
+  });
+
+  chunkify.loop(3, fn, {chunk: 3}).then(null, (rejection) => {
+    t.deepEquals(rejection, {error, index: 1});
+    t.equals(fn.callCount, 2);
+    t.end()
+  })
+});
+
+test('should not yield after `chunk` iterations if processing is complete', t => {
+  let fn = sinon.spy();
+
+  tick({
+    delay: 2000,
+
+    before_tick() {
+      chunkify.loop(3, fn, {chunk: 3, delay: 1000});
+      t.equals(fn.callCount, 3)
+    },
+
+    after_tick() {
+      t.equals(fn.callCount, 3);
+      t.end()
+    }
+  });
+});
