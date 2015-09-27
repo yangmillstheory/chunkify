68e69da190b68450389b3eeccfab26d5b7e04437
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-26 23:19:23 -0700

    mv

--- src/array/each.js
+++ src/array/each.js
@@ -1,14 +1,33 @@
 import ChunkifyOptions from '../options'
 import _ from 'underscore'
 import {ok_usage} from './utilities'
-import {chunkify} from '../core'
+import chunks from '../chunks'
 
 
 const USAGE = 'Usage: chunkify.each(Array array, Function fn, [Object options])';
 
 let each = (array, fn, options = {}) => {
   ok_usage(array, fn, USAGE);
-  return chunkify(array, fn, ChunkifyOptions.of(options));
+  let {chunk, delay, scope} = ChunkifyOptions.of(options);
+  let iterator = chunks.of({chunk, range: array.length});
+  let resume = (resolve, reject) => {
+    let {value, done} = iterator.next();
+    if (done) {
+      return resolve();
+    }
+    let {index, ok_chunk} = value;
+    let item = array[index];
+    try {
+      fn.call(scope, item, index);
+    } catch (error) {
+      return reject({error, item, index})
+    }
+    if (ok_chunk) {
+      return setTimeout(resume, delay, resolve, reject)
+    }
+    resume(resolve, reject);
+  };
+  return new Promise(resume);
 };
 
 
