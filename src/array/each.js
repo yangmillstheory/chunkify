7d11bf221f1cd2da89aca97ad55d0e893d824578
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-26 22:57:20 -0700

    squashed merge - work from generators

--- src/array/each.js
+++ src/array/each.js
@@ -5,30 +5,11 @@ import {ok_usage} from './utilities'
 
 const USAGE = 'Usage: chunkify.each(Array array, Function fn, [Object options])';
 
+import {chunkify} from '../core'
+
 let iterate = (array, fn, options = {}) => {
   ok_usage(array, fn, USAGE);
-  let {chunk, delay, scope} = ChunkifyOptions.of(options);
-  let index = 0;
-  let final = array.length;
-  let incomplete = () => {
-    return index < final;
-  };
-  var process_chunk = (resolve, reject) => {
-    while (incomplete()) {
-      let item = array[index];
-      try {
-        fn.call(scope, item, index)
-      } catch (error) {
-        return reject({error, item, index});
-      }
-      index++;
-      if (incomplete() && ((index % chunk) === 0)) {
-        return setTimeout(process_chunk.bind(null, resolve, reject), delay);
-      }
-    }
-    resolve();
-  };
-  return new Promise(process_chunk)
+  return chunkify(array, fn, ChunkifyOptions.of(options));
 };
 
 
