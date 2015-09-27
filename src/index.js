227b9355267568debe98659c25cd13ef8a5c10e4
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-27 01:31:44 -0700

    export range

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import 'babel/polyfill'
 import array from './array'
 import loop from './loop'
+import range from './range'
 
 export default {
   each: array.each,
   map: array.map,
   reduce: array.reduce,
-  loop
+  loop,
+  range
 }
