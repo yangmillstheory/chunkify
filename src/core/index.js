e840b6dd1ede3d0418725b268d2076292d53d415
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-26 22:56:17 -0700

    core algorithm

---
+++ src/core/index.js
@@ -0,0 +1,27 @@
+import chunks from './chunks'
+
+
+let chunkify = (array, fn, options = {}) => {
+  let {chunk, delay, scope} = options;
+  let iterator = chunks.of(array.length, chunk);
+  let resume = (resolve, reject) => {
+    let {value, done} = iterator.next();
+    if (done) {
+      return resolve();
+    }
+    let {index, ok_chunk} = value;
+    let item = array[index];
+    try {
+      fn.call(scope, item, index);
+    } catch (error) {
+      return reject({error, item, index})
+    }
+    if (ok_chunk) {
+      return setTimeout(resume, delay, resolve, reject)
+    }
+    resume(resolve, reject);
+  };
+  return new Promise(resume);
+};
+
+export default {chunkify}
