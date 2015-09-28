f41019279cff71ab751ca9f7f6a62ecf64bf447f
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-27 19:25:25 -0700

    refresh bundle, rm site/js/index

--- site/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _dist = require('../../dist');
-
-var _dist2 = _interopRequireDefault(_dist);
-
-console.log(_dist2['default']);
