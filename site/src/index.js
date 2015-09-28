71e3eb9f0891e38eeb351b3c2f4310207d6bf7c7
Victor Alvarez <v.alvarez312@gmail.com>
2015-09-27 19:22:48 -0700

    angular; ChunkifyCtrl

--- site/src/index.js
+++ site/src/index.js
@@ -1,3 +1,46 @@
 import chunkify from '../../dist'
+import angular from 'angular'
 
 console.log(chunkify);
+console.log(angular);
+
+
+angular
+.module('chunkify-demo', [])
+.controller('ChunkifyCtrl', ($scope) => {
+
+    $scope.buttons = {
+      disabled: false
+    };
+
+    $scope.chunked = {
+      reduce: function() {
+        console.log('chunked reduce')
+      },
+      map: function() {
+        console.log('chunked map')
+      },
+      each: function() {
+        console.log('chunked each')
+      },
+      loop: function() {
+        console.log('chunked loop')
+      }
+    };
+
+    $scope.regular = {
+      reduce: function() {
+        console.log('regular reduce')
+      },
+      map: function() {
+        console.log('regular map')
+      },
+      each: function() {
+        console.log('regular each')
+      },
+      loop: function() {
+        console.log('regular loop')
+      }
+    };
+
+});
