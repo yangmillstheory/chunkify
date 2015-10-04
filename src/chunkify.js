c8d9336ae81ee95283fdd9dcc3f3d987613c5e15
Victor Alvarez <v.alvarez312@gmail.com>
2015-10-03 19:41:08 -0700

    cleaning

--- src/chunkify.js
+++ src/chunkify.js
@@ -1,36 +1,29 @@
-let pending_delay_error = (index) => {
-  let pending_delay = `pending delay at index: ${index}; `;
-  pending_delay += `wait ${delay} milliseconds before `;
-  pending_delay += `further invocations of .next()`;
-  throw new Error(pending_delay)
-};
-
-// Return values from the range `start` to `final` synchronously when between
-// intervals of size `chunk`.
+// Return values from the range `start` to `final` synchronously
+// when between intervals of size `chunk`.
 //
-// Otherwise, return a promise that resolves in `delay` milliseconds.
+// On the chunk boundary, return a promise that resolves in `delay` milliseconds.
 //
-// An error will be thrown in case the iterator is advanced before a
-// pending promise has resolved.
+// An error will be thrown in case an iterator is advanced before a pending
+// promise has resolved.
 function* chunkify(start, final, chunk, delay) {
-  let delayed = null;
-  let pending = null;
+  let delayed = false;
+  let pending_delay_error = (index) => {
+    let pending_delay = `pending delay at index ${index}; `;
+    pending_delay += `wait ${delay} milliseconds before `;
+    pending_delay += `further invocations of .next()`;
+    throw new Error(pending_delay)
+  };
   function* do_delay() {
     yield new Promise(resolve => {
-      setTimeout(() => {
-        delayed = false;
-        pending = false;
-        resolve();
-      }, delay)
+      delayed = true;
+      setTimeout(() => { resolve(); delayed = false; }, delay)
     })
   }
   for (let index = start; index < final; index++) {
     if ((index > start) && (index % (start + chunk) === 0)) {
-      delayed = true;
-      pending = true;
       yield* do_delay()
     }
-    if (delayed && pending) {
+    if (delayed) {
       pending_delay_error(index);
     }
     yield index
