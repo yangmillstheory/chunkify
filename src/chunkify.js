8fadc42c9711d08efb182eb0d865c51fd39645af
Victor Alvarez <v.alvarez312@gmail.com>
2015-10-03 19:11:07 -0700

    Throw error if iterator advances if delay is pending

--- src/chunkify.js
+++ src/chunkify.js
@@ -1,7 +1,37 @@
+let pending_delay_error = (index) => {
+  let pending_delay = `pending delay at index: ${index}; `;
+  pending_delay += `wait ${delay} milliseconds before `;
+  pending_delay += `further invocations of .next()`;
+  throw new Error(pending_delay)
+};
+
+// Return values from the range `start` to `final` synchronously when between
+// intervals of size `chunk`.
+//
+// Otherwise, return a promise that resolves in `delay` milliseconds.
+//
+// An error will be thrown in case the iterator is advanced before a
+// pending promise has resolved.
 function* chunkify(start, final, chunk, delay) {
+  let delayed = null;
+  let pending = null;
+  function* do_delay() {
+    yield new Promise(resolve => {
+      setTimeout(() => {
+        delayed = false;
+        pending = false;
+        resolve();
+      }, delay)
+    })
+  }
   for (let index = start; index < final; index++) {
     if ((index > start) && (index % (start + chunk) === 0)) {
-      yield new Promise(resolve => setTimeout(resolve, delay))
+      delayed = true;
+      pending = true;
+      yield* do_delay()
+    }
+    if (delayed && pending) {
+      pending_delay_error(index);
     }
     yield index
   }
