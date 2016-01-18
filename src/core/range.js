cbbd72bc2809093943ea2bd5b56b808a2c4f2249
Victor Alvarez <valvarez@tableau.com>
2016-01-17 20:19:19 -0800

    rm import; rm _, clean

--- src/core/range.js
+++ src/core/range.js
@@ -1,22 +1,20 @@
 import interval from './interval'
-import ChunkifyOptions from '../options'
-import _ from 'underscore'
+import {
+  isFunction,
+  isNumber
+} from '../utility'
 
 
-const USAGE = 'Usage: chunkify.range(Function fn, Number range, [Object options])';
 
-let ok_usage = (fn, range) => {
-  if (!_.isFunction(fn)) {
+let checkUsage = (fn, range) => {
+  if (!isFunction(fn)) {
     throw new Error(`${USAGE} - bad fn; not a function`);
-  } else if (!_.isNumber(range)) {
+  } else if (!isNumber(range)) {
     throw new Error(`${USAGE} - bad range; not a number`);
   }
 };
 
-let range = (fn, range, options = {}) => {
-  ok_usage(fn, range);
-  delete options.start;
-  return interval(fn, range, options);
+  checkUsage(fn, range);
 };
 
 export default range
