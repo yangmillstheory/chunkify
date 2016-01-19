32c698a408e977a4f434b45c92d832c72f51241f
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-18 20:39:04 -0800

    re-write gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,113 @@
 let gulp = require('gulp');
+let karma = require('karma');
+let tslint = require('gulp-tslint');
 let ts = require('gulp-typescript');
 
+
+/////////
+// config
 
 const TS_PROJECT = ts.createProject('tsconfig.json', {
   typescript: require('typescript')
 });
 
-gulp.task('build', () => {
-  
-  return gulp.src(
-    [
-      `src/**/*.js`,
-      `!src/**/*.spec.js`,
-      `!src/**/test-utility.js`
-    ]
-  )
-  .pipe(ts(TS_PROJECT))
-  .pipe(gulp.dest('dist'));
+const TS = [
+  'src/**/*.ts',
+  'src/**/test-utility.ts',
+  '!**/*.spec.ts'
+];
+
+const SPEC = ['**/*.spec.ts']; 
+
+
+//////////
+// compile
+
+let compileStream = (globs) => {
+  return gulp
+    .src(globs)
+    .pipe(ts(TS_PROJECT))
+    .pipe(gulp.dest('dist'));
+};
+
+gulp.task('compile:ts', () => {
+  return compileStream(TS);
+});
+
+gulp.task('compile:spec', () => {
+  return compileStream(SPEC);
+});
+
+gulp.task('compile', gulp.parallel('compile:ts', 'compile:spec'))
+
+
+
+///////
+// lint
+
+let lintStream = (globs, rules) => {
+  return gulp.src(globs)
+    .pipe(tslint({
+      configuration: {
+        tslint: require('tslint'),
+        rules: rules,
+      } 
+    }))
+    .pipe(tslint.report('verbose', {
+      summarizeFailureOutput: true,
+      emitError: false
+    }));
+};
+
+gulp.task('lint:ts', () => {
+  return lintStream(TS);
+});
+
+gulp.task('lint:spec', () => {
+  return lintStream(SPEC);
+});
+
+gulp.task('lint', gulp.parallel('lint:ts', 'lint:spec'));
+
+
+///////
+// test
+
+let startKarmaServer = (options, done) => {
+  options = Object.assign(options || {}, {
+    configFile: 'karma.conf.unit.js',
+  });
+  new karma.Server(options, exitCode => {
+    done();
+    process.exit(exitCode);
+  }).start();
+};
+
+gulp.task('test', done => {
+  startKarmaServer({
+    singleRun: true,
+    autoWatch: false,
+  }, done);
+});
+
+gulp.task('tdd', done => {
+  startKarmaServer({
+    singleRun: false,
+    autoWatch: true,
+  }, done);
 });
 
 
-gulp.task('default', gulp.series('build'));
+/////////////////////////
+// continuous development
+
+gulp.task('watch', done => {
+  gulp.watch(TS, gulp.series(
+    'compile:ts', 
+    'lint:ts'));
+  gulp.watch(SPEC, gulp.series(
+    'compile:spec', 
+    'lint:spec'));
+});
+
+gulp.task('dev', gulp.parallel('tdd', 'watch'));
