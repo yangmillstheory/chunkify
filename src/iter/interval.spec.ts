3d0f3c9e70b8099ec085539ad9f4d916d332ad4a
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-24 00:40:53 -0800

    pass some core tests

--- src/iter/interval.spec.ts
+++ src/iter/interval.spec.ts
@@ -55,10 +55,21 @@ describe('interval', () => {
     expect(interval(() => {}, 0, 1)).to.be.instanceOf(Promise);
   });
 
+  it('should invoke fn the correct number of times', done => {
+    let fn = spy();
+
+    interval(fn, 1, 4, {chunk: 3})
+      .then(() => {
+        expect(fn.callCount).to.equal(3);
+        done();
+      })
+      .catch(done);
+  });
+
   it('should invoke fn with a default scope', done => {
     let fn = spy();
 
-    interval(fn, 0, 3, {chunk: 3})
+    interval(fn, 1, 4, {chunk: 3})
       .then(() => {
         expect(fn.alwaysCalledOn(null)).to.be.ok;
         done();
@@ -70,7 +81,7 @@ describe('interval', () => {
     let fn = spy();
     let scope = spy();
 
-    interval(fn, 0, 3, {chunk: 3, scope})
+    interval(fn, 1, 4, {chunk: 3, scope})
       .then(() => {
         expect(fn.alwaysCalledOn(scope)).to.be.ok;
         done();
@@ -78,91 +89,83 @@ describe('interval', () => {
       .catch(done);
   });
 
+  it('should invoke fn with the correct loop index', done => {
+    let fn = spy();
+
+    interval(fn, 1, 4, {chunk: 3})
+      .then(() => {
+        expect(fn.callCount).to.equal(3);
+        expect(fn.getCall(0).calledWithExactly(1)).to.be.ok;
+        expect(fn.getCall(1).calledWithExactly(2)).to.be.ok;
+        expect(fn.getCall(2).calledWithExactly(3)).to.be.ok;
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should yield for at least "delay" ms after "chunk" iterations', done => {
+    let fn = spy();
+
+    tick({
+      delay: 9,
+
+      before() {
+        interval(fn, 1, 5, {chunk: 3, delay: 10});
+        expect(fn.callCount).to.equal(3);
+      },
+
+      after() {
+        // no more invocations
+        expect(fn.callCount).to.equal(3);
+        done();
+      },
+    });
+  });
+
+  it('should start again after "delay" milliseconds', done => {
+    let fn = spy();
+
+    tick({
+      delay: 11,
+
+      before() {
+        interval(fn, 1, 5, {chunk: 3, delay: 10});
+        expect(fn.callCount).to.equal(3);
+      },
+
+      after() {
+        expect(fn.callCount).to.equal(4);
+        expect(fn.lastCall.calledWithExactly(4));
+        done();
+      },
+    });
+  });
+
+  it('should resolve with undefined', done => {
+    let fn = spy();
+
+    tick({
+      delay: 11,
+
+      before() {
+        let promise = interval(fn, 1, 5, {chunk: 3, delay: 10});
+        expect(fn.callCount).to.equal(3);
+        return promise;
+      },
+
+      after(promise) {
+        expect(fn.callCount).to.equal(4);
+        promise
+          .then(result => {
+            expect(result).not.to.exist;
+            done();
+          })
+          .catch(done);
+      },
+    });
+  });
 });
 
-
-
-
-// test('should invoke fn with the provided scope', () => {
-//   let fn = sinon.spy();
-//   let scope = {};
-
-//   chunkify.interval(fn, 3, {start: 1, chunk: 3, scope}).then(() => {
-//     t.ok(fn.alwaysCalledOn(scope));
-//     t.end()
-//   });
-// });
-
-// test('should invoke fn with the correct loop index', () => {
-//   let fn = sinon.spy();
-
-//   chunkify.interval(fn, 4, {start: 1, chunk: 3}).then(() => {
-//     t.equals(fn.callCount, 3);
-//     t.deepEqual(fn.getCall(0).args, [1]);
-//     t.deepEqual(fn.getCall(1).args, [2]);
-//     t.deepEqual(fn.getCall(2).args, [3]);
-//     t.end()
-//   })
-// });
-
-// test('should yield for at least `delay` ms after `chunk` iterations', () => {
-//   let fn = sinon.spy();
-
-//   tick({
-//     delay: 9,
-
-//     beforeTick() {
-//       chunkify.interval(fn, 5, {start: 1, chunk: 3, delay: 10});
-//       t.equals(fn.callCount, 3);
-//     },
-
-//     afterTick() {
-//       t.equals(fn.callCount, 3);
-//       t.end();
-//     }
-//   });
-// });
-
-// test('should start again after `delay` milliseconds from last yielding', () => {
-//   let fn = sinon.spy();
-
-//   tick({
-//     delay: 11,
-
-//     beforeTick() {
-//       chunkify.interval(fn, 5, {start: 1, chunk: 3, delay: 10});
-//       t.equals(fn.callCount, 3);
-//     },
-
-//     afterTick() {
-//       t.equals(fn.callCount, 4);
-//       t.deepEquals(fn.lastCall.args, [4]);
-//       t.end();
-//     }
-//   });
-// });
-
-// test('should resolve with undefined', () => {
-//   let fn = sinon.spy();
-
-//   tick({
-//     delay: 11,
-
-//     beforeTick() {
-//       let promise = chunkify.interval(fn, 5, {start: 1, chunk: 3, delay: 10});
-//       t.equals(fn.callCount, 3);
-//       return promise
-//     },
-
-//     afterTick(promise) {
-//       t.equals(fn.callCount, 4);
-//       promise.then((result) => {
-//         t.equals(result, undefined);
-//         t.end()
-//       })
-//     }
-//   });
-// });
 
 // test('should reject the promise with rejection object and stop processing', () => {
 //   let error = {};
@@ -179,7 +182,7 @@ describe('interval', () => {
 //   })
 // });
 
-// test('should not yield after `chunk` iterations if processing is complete', () => {
+// test('should not yield after "chunk"" iterations if processing is complete', () => {
 //   let fn = sinon.spy();
 
 //   tick({
