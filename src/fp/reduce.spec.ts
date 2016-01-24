c853604d22107cf6f286474b70b91ce146cce6bb
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-24 15:02:41 -0800

    finish reduce tests

--- src/fp/reduce.spec.ts
+++ src/fp/reduce.spec.ts
@@ -86,140 +86,147 @@ describe('reduce', () => {
       .catch(done);
   });
 
-// it('should invoke tReducer with the memo as the first item when no memo is given', () => {
-//   let tArray = ['A', 'B', 'C'];
-//   let identity = spy(memo => {
-//     return memo;
-//   });
+  it('should use the first element in tArray as the memo when no memo is given', done => {
+    let tArray = ['A', 'B', 'C'];
+    let tReducer = spy(memo => {
+      return memo;
+    });
 
-//   reduce(array, identity, {chunk: 2}).then(() => {
-//     // Note the reduced call count!
-//     t.equals(identity.callCount, 2);
-//     t.deepEqual(identity.getCall(0).args, ['A', 'B', 1, array]);
-//     t.deepEqual(identity.getCall(1).args, ['A', 'C', 2, array]);
-//     done()
-//   })
-// });
+    reduce(tArray, tReducer, {chunk: 2}).then(() => {
+      // note the smaller call count; not 3 like above
+      expect(tReducer.callCount).to.equal(2);
+      expect(tReducer.getCall(0).args).to.deep.equal(['A', 'B', 1, tArray]);
+      expect(tReducer.getCall(1).args).to.deep.equal(['A', 'C', 2, tArray]);
+      done();
+    })
+    .catch(done);
+  });
 
-// it('should yield for at least `delay` ms after `chunk` iterations', () => {
-//   let tReducer = spy();
+  it('should yield for at least "delay" ms after "chunk" iterations', done => {
+    let tReducer = spy();
 
-//   tick({
-//     delay: 9,
+    tick({
+      delay: 9,
 
-//     beforeTick() {
-//       reduce(['A', 'B', 'C', 'D'], tReducer, {memo: '', chunk: 3, delay: 10});
-//       t.equals(tReducer.callCount, 3);
-//     },
+      before() {
+        reduce(['A', 'B', 'C', 'D'], tReducer, {chunk: 3, delay: 10}, '');
+        expect(tReducer.callCount).to.equal(3);
+      },
 
-//     afterTick() {
-//       t.equals(tReducer.callCount, 3);
-//       done();
-//     }
-//   });
-// });
+      after() {
+        expect(tReducer.callCount).to.equal(3);
+        done();
+      },
+    });
+  });
 
-// it('should start again after `delay` milliseconds from last yielding', () => {
-//   let tReducer = spy(memo => {
-//     return memo
-//   });
-//   let tArray = ['A', 'B', 'C', 'D'];
+  it('should start again after "delay" milliseconds from last yielding', done => {
+    let tReducer = spy(memo => {
+      return memo;
+    });
+    let tArray = ['A', 'B', 'C', 'D'];
 
-//   tick({
-//     delay: 11,
+    tick({
+      delay: 11,
 
-//     beforeTick() {
-//       reduce(array, tReducer, {memo: '', chunk: 3, delay: 10});
-//       t.equals(tReducer.callCount, 3);
-//     },
+      before() {
+        reduce(tArray, tReducer, {chunk: 3, delay: 10}, '');
+        expect(tReducer.callCount).to.equal(3);
+      },
 
-//     afterTick() {
-//       t.equals(tReducer.callCount, 4);
-//       t.deepEqual(tReducer.getCall(3).args, ['', 'D', 3, array]);
-//       done();
-//     }
-//   });
-// });
+      after() {
+        expect(tReducer.callCount).to.equal(4);
+        expect(tReducer.getCall(3).args).to.deep.equal(['', 'D', 3, tArray]);
+        done();
+      },
+    });
+  });
 
-// it('should resolve with the reduced result from a default memo', () => {
-//   let tReducer = spy((memo, letter, index, array) => {
-//     return `${memo.toLowerCase()}+${letter.toLowerCase()}`
-//   });
+  it('should resolve with the reduced result from a default memo', done => {
+    let tReducer = spy((memo, letter, index, tArray) => {
+      return `${memo.toLowerCase()}+${letter.toLowerCase()}`;
+    });
 
-//   tick({
-//     delay: 11,
+    tick({
+      delay: 11,
 
-//     beforeTick() {
-//       let promise = reduce(['A', 'B', 'C', 'D'], tReducer, {chunk: 3, delay: 10});
-//       t.equals(tReducer.callCount, 2);
-//       return promise
-//     },
+      before() {
+        let reducePromise = reduce(['A', 'B', 'C', 'D'], tReducer, {chunk: 3, delay: 10});
+        expect(tReducer.callCount).to.equal(2);
+        return reducePromise;
+      },
 
-//     afterTick(promise) {
-//       t.equals(tReducer.callCount, 3);
-//       promise.then((result) => {
-//         t.deepEquals(result, 'a+b+c+d');
-//         done()
-//       })
-//     }
-//   });
-// });
+      after(reducePromise) {
+        expect(tReducer.callCount).to.equal(3);
+        reducePromise
+          .then(result => {
+            expect(result).to.deep.equal('a+b+c+d');
+            done();
+          })
+          .catch(done);
+      },
+    });
+  });
 
-// it('should resolve with the reduced result from a given memo', () => {
-//   let tReducer = spy((memo, letter, index, array) => {
-//     return `${memo.toLowerCase()}+${letter.toLowerCase()}`
-//   });
+  it('should resolve with the reduced result from a given memo', done => {
+    let tReducer = spy((memo, letter, index, array) => {
+      return `${memo.toLowerCase()}+${letter.toLowerCase()}`;
+    });
 
-//   tick({
-//     delay: 11,
+    tick({
+      delay: 11,
 
-//     beforeTick() {
-//       let promise = reduce(
-//         ['A', 'B', 'C', 'D'], tReducer, {chunk: 3, delay: 10, memo: 'MEMO'});
-//       t.equals(tReducer.callCount, 3);
-//       return promise
-//     },
+      before() {
+        let reducePromise = reduce(['A', 'B', 'C', 'D'], tReducer, {chunk: 3, delay: 10}, 'MEMO');
+        expect(tReducer.callCount).to.equal(3);
+        return reducePromise;
+      },
 
-//     afterTick(promise) {
-//       t.equals(tReducer.callCount, 4);
-//       promise.then((result) => {
-//         t.deepEquals(result, 'memo+a+b+c+d');
-//         done()
-//       })
-//     }
-//   });
-// });
+      after(reducePromise) {
+        expect(tReducer.callCount).to.equal(4);
+        reducePromise
+          .then(result => {
+            expect(result).to.deep.equal('memo+a+b+c+d');
+            done();
+          })
+          .catch(done);
+      },
+    });
+  });
 
-// it('should reject the promise with rejection object and stop processing', () => {
-//   let error = {};
-//   let tReducer = spy((memo, letter) => {
-//     if (letter === 'B') {
-//       throw error;
-//     }
-//   });
+  it('should reject the promise with rejection object and stop processing', done => {
+    let error = {};
+    let tReducer = spy((memo, letter) => {
+      if (letter === 'B') {
+        throw error;
+      }
+    });
 
-//   reduce(['A', 'B', 'C'], tReducer, {chunk: 3, memo: ''}).then(null, (rejection) => {
-//     t.deepEquals(rejection, {error, item: 'B', index: 1});
-//     t.equals(tReducer.callCount, 2);
-//     done()
-//   })
-// });
+    reduce(['A', 'B', 'C'], tReducer, {chunk: 3}, '')
+      .catch(rejection => {
+        expect(rejection).to.deep.equal({error, item: 'B', index: 1});
+        expect(tReducer.callCount).to.equal(2);
+        done();
+      });
+  });
 
-// it('should not yield after `chunk` iterations if processing is complete', () => {
-//   let tReducer = spy();
+  it('should not yield after "chunk" iterations if processing is complete', done => {
+    let tReducer = spy();
 
-//   tick({
-//     delay: 20,
+    tick({
+      delay: 20,
 
-//     beforeTick() {
-//       reduce(['A', 'B', 'C'], tReducer, {chunk: 3, delay: 10, memo: ''});
-//       t.equals(tReducer.callCount, 3)
-//     },
+      before() {
+        reduce(['A', 'B', 'C'], tReducer, {chunk: 3, delay: 10}, '');
+        expect(tReducer.callCount).to.equal(3);
+      },
 
-//     afterTick() {
-//       t.equals(tReducer.callCount, 3);
-//       done()
-//     }
-//   });
-// });
+      after() {
+        expect(tReducer.callCount).to.equal(3);
+        done();
+      },
+    });
+
+  });
+
 });
