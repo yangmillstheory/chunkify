754a9d22a9b92d5cb15acb817217b6bcd63e8f50
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-24 11:58:48 -0800

    naming

--- src/iter/interval.spec.ts
+++ src/iter/interval.spec.ts
@@ -8,7 +8,7 @@ import proxyquire from 'proxyquire';
 describe('interval', () => {
 
   it('should require a function', () => {
-    for (let fn of [
+    for (let indexConsumer of [
       1,
       'string',
       {},
@@ -16,7 +16,7 @@ describe('interval', () => {
       null,
       undefined,
     ]) {
-      expect(() => { interval(fn); }).throws(/Expected function; got /);
+      expect(() => { interval(indexConsumer); }).throws(/Expected function; got /);
     }
   });
 
@@ -67,106 +67,106 @@ describe('interval', () => {
     expect(interval(() => {}, 0, 1)).to.be.instanceOf(Promise);
   });
 
-  it('should invoke fn the correct number of times', done => {
-    let fn = spy();
+  it('should invoke indexConsumer the correct number of times', done => {
+    let indexConsumer = spy();
 
-    interval(fn, 1, 4, {chunk: 3})
+    interval(indexConsumer, 1, 4, {chunk: 3})
       .then(() => {
-        expect(fn.callCount).to.equal(3);
+        expect(indexConsumer.callCount).to.equal(3);
         done();
       })
       .catch(done);
   });
 
-  it('should invoke fn with a default scope', done => {
-    let fn = spy();
+  it('should invoke indexConsumer with a default scope', done => {
+    let indexConsumer = spy();
 
-    interval(fn, 1, 4, {chunk: 3})
+    interval(indexConsumer, 1, 4, {chunk: 3})
       .then(() => {
-        expect(fn.alwaysCalledOn(null)).to.be.ok;
+        expect(indexConsumer.alwaysCalledOn(null)).to.be.ok;
         done();
       })
       .catch(done);
   });
 
-  it('should invoke fn with a provided scope', done => {
-    let fn = spy();
+  it('should invoke indexConsumer with a provided scope', done => {
+    let indexConsumer = spy();
     let scope = spy();
 
-    interval(fn, 1, 4, {chunk: 3, scope})
+    interval(indexConsumer, 1, 4, {chunk: 3, scope})
       .then(() => {
-        expect(fn.alwaysCalledOn(scope)).to.be.ok;
+        expect(indexConsumer.alwaysCalledOn(scope)).to.be.ok;
         done();
       })
       .catch(done);
   });
 
-  it('should invoke fn with the correct loop index', done => {
-    let fn = spy();
+  it('should invoke indexConsumer with the correct loop index', done => {
+    let indexConsumer = spy();
 
-    interval(fn, 1, 4, {chunk: 3})
+    interval(indexConsumer, 1, 4, {chunk: 3})
       .then(() => {
-        expect(fn.callCount).to.equal(3);
-        expect(fn.getCall(0).calledWithExactly(1)).to.be.ok;
-        expect(fn.getCall(1).calledWithExactly(2)).to.be.ok;
-        expect(fn.getCall(2).calledWithExactly(3)).to.be.ok;
+        expect(indexConsumer.callCount).to.equal(3);
+        expect(indexConsumer.getCall(0).calledWithExactly(1)).to.be.ok;
+        expect(indexConsumer.getCall(1).calledWithExactly(2)).to.be.ok;
+        expect(indexConsumer.getCall(2).calledWithExactly(3)).to.be.ok;
         done();
       })
       .catch(done);
   });
 
   it('should yield for at least "delay" ms after "chunk" iterations', done => {
-    let fn = spy();
+    let indexConsumer = spy();
 
     tick({
       delay: 9,
 
       before() {
-        interval(fn, 1, 5, {chunk: 3, delay: 10});
-        expect(fn.callCount).to.equal(3);
+        interval(indexConsumer, 1, 5, {chunk: 3, delay: 10});
+        expect(indexConsumer.callCount).to.equal(3);
       },
 
       after() {
         // no more invocations
-        expect(fn.callCount).to.equal(3);
+        expect(indexConsumer.callCount).to.equal(3);
         done();
       },
     });
   });
 
   it('should start again after "delay" milliseconds', done => {
-    let fn = spy();
+    let indexConsumer = spy();
 
     tick({
       delay: 11,
 
       before() {
-        interval(fn, 1, 5, {chunk: 3, delay: 10});
-        expect(fn.callCount).to.equal(3);
+        interval(indexConsumer, 1, 5, {chunk: 3, delay: 10});
+        expect(indexConsumer.callCount).to.equal(3);
       },
 
       after() {
-        expect(fn.callCount).to.equal(4);
-        expect(fn.lastCall.calledWithExactly(4));
+        expect(indexConsumer.callCount).to.equal(4);
+        expect(indexConsumer.lastCall.calledWithExactly(4));
         done();
       },
     });
   });
 
   it('should resolve with undefined', done => {
-    let fn = spy();
+    let indexConsumer = spy();
 
     tick({
       delay: 11,
 
       before() {
-        let promise = interval(fn, 1, 5, {chunk: 3, delay: 10});
-        expect(fn.callCount).to.equal(3);
+        let promise = interval(indexConsumer, 1, 5, {chunk: 3, delay: 10});
+        expect(indexConsumer.callCount).to.equal(3);
         return promise;
       },
 
       after(promise) {
-        expect(fn.callCount).to.equal(4);
+        expect(indexConsumer.callCount).to.equal(4);
         promise
           .then(result => {
             expect(result).not.to.exist;
@@ -179,33 +179,33 @@ describe('interval', () => {
 
   it('should reject the promise with rejection object and stop processing', done => {
     let error = new Error('example error');
-    let fn = spy(index => {
+    let indexConsumer = spy(index => {
       if (index === 2) {
         throw error;
       }
     });
 
-    interval(fn, 1, 5, {chunk: 3})
+    interval(indexConsumer, 1, 5, {chunk: 3})
       .catch(rejection => {
         expect(rejection).to.deep.equal({error, index: 2});
-        expect(fn.callCount).to.equal(2);
+        expect(indexConsumer.callCount).to.equal(2);
         done();
       });
   });
 
   it('should not yield after "chunk" iterations if processing is complete', done => {
-    let fn = spy();
+    let indexConsumer = spy();
 
     tick({
       delay: 20,
 
       before() {
-        interval(fn, 1, 4, {chunk: 3, delay: 10});
-        expect(fn.callCount).to.equal(3);
+        interval(indexConsumer, 1, 4, {chunk: 3, delay: 10});
+        expect(indexConsumer.callCount).to.equal(3);
       },
 
       after() {
-        expect(fn.callCount).to.equal(3);
+        expect(indexConsumer.callCount).to.equal(3);
         done();
       },
     });
