204dd534e5320ecbe48e2c1c72fd805001f306bd
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-24 15:16:51 -0800

    remove useless interfaces

--- chunkify.d.ts
+++ chunkify.d.ts
@@ -6,18 +6,6 @@ declare interface IChOptions {
   scope?: Object;
 }
 
-declare interface IChReduceOptions<T> extends IChOptions {
-  memo?: T;
-}
-
-declare interface IChReducer<T, U> extends Function {
-  (memo: U, item: T, index: number, array: T[]): U;
-}
-
-declare interface IChMapper<T> extends Function {
-  (item: T): T;
-}
-
 declare interface IChPause { resume(fn: () => void); }
 
 declare module 'chunkify' {
