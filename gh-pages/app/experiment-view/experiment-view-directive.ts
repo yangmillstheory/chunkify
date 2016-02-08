a50bfe6e4d36b80f96bd9e705da6ff3bcf1b8173
Victor Alvarez <v.alvarez312@gmail.com>
2016-02-07 22:31:23 -0800

    fixing code view

--- gh-pages/app/experiment-view/experiment-view-directive.ts
+++ gh-pages/app/experiment-view/experiment-view-directive.ts
@@ -12,12 +12,13 @@ export var experimentView = function(): ng.IDirective {
 const RANGE = _.range(0.5 * Math.pow(10, 5));
 <strong>let chunk = {{ experiment.options.chunk }};</strong>
 <strong>let delay = {{ experiment.options.delay }};</strong>
-let indexConsumer = function(index: number): void {
+let indexConsumer = function(j: number): number {
   let i = 0;
   let max = largeRandomInt();
   while (i < max) {
     i++;
   }
+  return j;
 };
   </pre>
   <pre>
