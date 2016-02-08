b14aa1dbe63e3c0b125fa03a1f3e06727627e562
Victor Alvarez <v.alvarez312@gmail.com>
2016-02-07 21:10:19 -0800

    fix x-* attribute

--- gh-pages/common/resizing-container-directive.ts
+++ gh-pages/common/resizing-container-directive.ts
@@ -6,8 +6,8 @@ let $ = jQuery.fn.constructor;
 interface IResizingContainerAttrs {
   minWidth: string;
   minHeight: string;
-  xPadding: string;
-  yPadding: string;
+  offsetX: string;
+  offsetY: string;
 }
 
 
@@ -17,8 +17,8 @@ interface IResizingContainerAttrs {
  * @restrict E
  * @param {number} minWidth:  the minimum width to resize to
  * @param {number} minHeight: the minimum height to resize to
- * @param {number} xPadding: subtracted from the resize width
- * @param {number} yPadding: subtracted from the resize height
+ * @param {number} offsetX: subtracted from the resize width
+ * @param {number} offsetY: subtracted from the resize height
  */
 export var resizingContainer = function($window: ng.IWindowService): ng.IDirective {
   return {
@@ -33,14 +33,14 @@ export var resizingContainer = function($window: ng.IWindowService): ng.IDirecti
     ): void {
       let minWidth = parseInt(attrs.minWidth, 10);
       let minHeight = parseInt(attrs.minHeight, 10);
-      let xPadding = parseInt(attrs.xPadding, 10);
-      let yPadding = parseInt(attrs.yPadding, 10);
+      let offsetX = parseInt(attrs.offsetX, 10);
+      let offsetY = parseInt(attrs.offsetY, 10);
       let documentElement = $window.document.documentElement;
       let $element = $(element);
       let resize = function(): boolean {
         $element.css({
-          width: Math.max(documentElement.clientWidth - xPadding, minWidth),
-          height: Math.max(documentElement.clientHeight - yPadding, minHeight)
+          width: Math.max(documentElement.clientWidth - offsetX, minWidth),
+          height: Math.max(documentElement.clientHeight - offsetY, minHeight)
         });
         return true;
       };
