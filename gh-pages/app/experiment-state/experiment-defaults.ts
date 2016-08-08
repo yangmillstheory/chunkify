048714dfb7f2edf8156ddb836e895dc049e74aec
VIctor Alvarez <v.alvarez312@gmail.com>
2016-08-07 20:23:12 -0700

    WIP

--- gh-pages/app/experiment-state/experiment-defaults.ts
+++
@@ -1,6 +0,0 @@
-import * as _ from 'lodash';
-
-
-export const RANGE = _.range(0.5 * Math.pow(10, 5));
-export const CHUNK = 100;
-export const DELAY = 50;
--- gh-pages/common/title-case-filter.ts
+++
@@ -1,8 +0,0 @@
-export var titlecase = function(): (text: string) => string {
-  return function(text: string): string {
-    if (typeof text !== 'string') {
-      throw new TypeError(`Expected string ,got ${typeof text}`);
-    }
-    return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
-  };
-};
